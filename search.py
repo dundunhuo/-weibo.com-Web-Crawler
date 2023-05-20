e01d9356222611c024ac1227f4600ce9178c1f3b
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2023-05-20 21:29:27 +1200

    v0.3.1

--- search.py
+++ search.py
@@ -12,9 +12,9 @@ sess = Session()
 sess.trust_env = False
 
 
-def query_weibo_first_page(search_string, header, st, et, rest_time=(2, 5)):
-    url = f"https://s.weibo.com/weibo?q={quote(search_string)}&typeall=1&suball=1&timescope=custom:" \
-          f"{st[0]}-{st[1]}-{st[2]}-{st[3]}:{et[0]}-{et[1]}-{et[2]}-{et[3]}&Refer=g"
+def get_first_page(search_string, header, st, et, rest_time=(2, 5)):
+    url = f"https://s.weibo.com/weibo?q={quote(search_string)}&typeall=1&suball=1&timescope=custom%3A" \
+          f"{st[0]}-{st[1]}-{st[2]}-{st[3]}%3A{et[0]}-{et[1]}-{et[2]}-{et[3]}&Refer=g"
     response = sess.get(url, headers=header, verify=True)
     assert response.status_code == 200, f'Response code is {response.status_code} for {url}'
     sleep(round(random.uniform(rest_time[0], rest_time[1]), 2))
@@ -25,7 +25,7 @@ def query_weibo_first_page(search_string, header, st, et, rest_time=(2, 5)):
     return response, max_page
 
 
-def query_weibo(search_string, header, st, et, page, rest_time=(2, 5)):
+def get_subseq_page(search_string, header, st, et, page, rest_time=(2, 5)):
     url = f"https://s.weibo.com/weibo?q={quote(search_string)}&typeall=1&suball=1&timescope=custom:" \
           f"{st[0]}-{st[1]}-{st[2]}-{st[3]}:{et[0]}-{et[1]}-{et[2]}-{et[3]}&Refer=g&page={page}"
     response = sess.get(url, headers=header, verify=True)
@@ -42,7 +42,7 @@ def query_weibo(search_string, header, st, et, page, rest_time=(2, 5)):
     return response
 
 
-def get_posts(db, table, web_text):
+def get_posts(web_text, header, db, table, rest_time=(2, 5)):
     posts = web_text.find_all('div', {'class': 'card-wrap', 'action-type': 'feed_list_item'})
     posts_df = []
     for post in posts:
@@ -56,11 +56,12 @@ def get_posts(db, table, web_text):
         except AttributeError:
             pass
         try:
-            attributes['homepage'] = post.find('a', {'class': 'name'}).get('href')
+            # profile https://weibo.com/u/$user_id
+            attributes['user_id'] = post.find('a', {'class': 'name'}).get('href').split('/')[-1].split('?')[0]
         except AttributeError:
             pass
         try:
-            attributes['posted_time'] = post.find('p', {'class': 'from'}).find('a', {'suda-data': True}).text.strip()
+            attributes['posted_time'] = post.find('a', {'suda-data': re.compile('wb_time')}).text.strip()
         except AttributeError:
             pass
         try:
@@ -68,12 +69,28 @@ def get_posts(db, table, web_text):
         except AttributeError:
             pass
         try:
-            # TODO: get full text if found "展开c" in fetched content
-            attributes['content'] = post.find('p', {'class': 'txt', 'node-type': 'feed_list_content'}).text.strip()
-            attributes['content'] = re.sub('\u200b', '', attributes['content'])
-            attributes['content'] = re.sub('\ue627', '', attributes['content'])
+            # post https://weibo.com/$user_id/$weibo_id
+            attributes['weibo_id'] = post.find('a', text=re.compile('复制微博地址')).get(
+                '@click', '').split('/')[-1].split('?')[0]
         except AttributeError:
             pass
+        try:
+            unfold_link = post.find('a', {'action-type': "fl_unfold"})
+            if unfold_link and 'weibo_id' in attributes.keys():
+                response = sess.get(url="https://weibo.com/ajax/statuses/longtext", headers=header, verify=True,
+                                    params={'id': attributes['weibo_id']})
+                sleep(round(random.uniform(rest_time[0], rest_time[1]), 2))
+                if response.status_code == 200:
+                    attributes['content'] = response.json().get('data').get('longTextContent')
+        except AttributeError:
+            print()
+        if 'content' not in attributes.keys():
+            try:
+                attributes['content'] = post.find('p', {'class': 'txt', 'node-type': 'feed_list_content'}).text.strip()
+                attributes['content'] = re.sub('\u200b', '', attributes['content'])
+                attributes['content'] = re.sub('\ue627', '', attributes['content'])
+            except AttributeError:
+                pass
         # SQLite 3 doesn't support list
         # try:
         #     attributes['images'] = [x.get('src') for x in
@@ -97,14 +114,14 @@ def get_posts(db, table, web_text):
             pass
         posts_df.append(attributes)
     posts_df = pd.DataFrame(data=posts_df,
-                            columns=['avatar', 'nickname', 'homepage', 'posted_time', 'source', 'content',
+                            columns=['avatar', 'nickname', 'user_id', 'posted_time', 'source', 'weibo_id', 'content',
                                      'reposts', 'comments', 'likes'])
     connection = sqlite3.connect(db)
     posts_df.to_sql(table, connection, if_exists='append', index=False)
     connection.close()
 
 
-def search(db, table, query, start_time, end_time):
+def search(db, table, query, start_time, end_time, max_page=None):
     connection = sqlite3.connect(db)
     cookies = pd.read_sql_query('select * from cookies', connection)
     connection.close()
@@ -123,24 +140,18 @@ def search(db, table, query, start_time, end_time):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/113.0.0.0 Safari/537.36"
     }
-    response, max_page = query_weibo_first_page(
+    response, existed_page = get_first_page(
         search_string=query, header=chrome_113, st=start_time, et=end_time)
-    get_posts(db=db, table=table, web_text=response)
+    if max_page:
+        max_page = min(max_page, existed_page)
+    else:
+        max_page = existed_page
+    get_posts(db=db, table=table, header=chrome_113, web_text=response)
     pbar = tqdm(total=max_page)
     pbar.update(1)
     for page in range(2, max_page + 1):
-        response = query_weibo(search_string=query, header=chrome_113, st=start_time, et=end_time, page=page)
+        response = get_subseq_page(search_string=query, header=chrome_113, st=start_time, et=end_time, page=page)
         if response:
-            get_posts(db=db, table=table, web_text=response)
+            get_posts(db=db, table=table, header=chrome_113, web_text=response)
         pbar.update(1)
     pbar.close()
-
-
-if __name__ == '__main__':
-    search(
-        db="posts.db",
-        table="star_rail",
-        query="星穹铁道",
-        start_time=(2023, 4, 26, 0),
-        end_time=(2023, 4, 26, 3)
-    )
